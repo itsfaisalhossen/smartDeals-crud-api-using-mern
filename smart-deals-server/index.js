25eda95b406e48dbc7e3356f60e2f0004d5c6aea
itsfaisalhossen <itsfaisalhossen@gmail.com>
2025-11-05 20:10:50 +0200

    Added env file

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,8 +13,7 @@ app.get("/", (req, res) => {
   res.send("Smart server is running");
 });
 
-const uri =
-  "mongodb+srv://smartDBUser:kFXlOBXRtxQWDK8R@cluster0.uvhdimh.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvhdimh.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
